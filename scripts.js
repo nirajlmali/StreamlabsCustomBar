e6e8625825e5262405c8bcc2b35e5dc2d28d8179
Niraj Mali <nirajlmali@gmail.com>
2021-02-16 20:07:30 -0800

    Comments

--- scripts.js
+++ scripts.js
@@ -5,9 +5,17 @@ document.addEventListener('goalLoad', function(obj) {
     // obj.detail will contain information about the current goal
     // this will fire only once when the widget loads
     console.log(obj.detail);
+
+    //Updates the title of the goal
     $('#title').html(obj.detail.title);
+
+    //Updates the current amount reached towards the goal
     $('#goal-current').text(obj.detail.amount.current);
+
+    //Updates the target amount
     $('#goal-total').text(obj.detail.amount.target);
+
+    //Updates the end date of the goal
     $('#goal-end-date').text(obj.detail.to_go.ends_at);
   });
   
